8a38dda9c11054f71dc9be0b0d310af41e889303
sti <sti@matera.com>
2022-06-26 15:52:01 -0300

    Teste 002

--- login_test.js
+++ login_test.js
@@ -17,7 +17,7 @@ Scenario('Login Test Example', async ({I}) => {
     I.waitForElement('.page-heading', 10)
     var title = await I.grabTitle()
     validacao.equal(title, 'Login - My Store')
-    //Teste
+    //Teste 001
 
     I.see('CREATE AN ACCOUNT')
 });
